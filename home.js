c06ffdda14f3e586fd16c3bb56d05f4724785ff5
uiwwnw Yoon <uiwwnwyoon@uiwwnwui-MacBook.local>
2018-06-27 17:11:55 +0900

    서버

--- home.js
+++
@@ -1,7 +0,0 @@
-var module = require('./custom_module');
-
-// 1. formatted 특수문자 %d를 사용해서 module.sum( ) 에서 리턴된 숫자값을 출력
-console.log('sum = %d,var1 = %s', module.sum(100),module.var1);
-
-// 2. formatted 특수문자 %s를 사용해서 module.var1의 문자값을 출력
-// console.log('var1 = %s' , module.var1);
